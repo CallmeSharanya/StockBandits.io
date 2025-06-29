e24138aaaa791e1abc3d9272f773f42de8dc5fb0
Harshitha40 <sharshitha.cs23@rvce.edu.in>
2025-06-29 12:00:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AlgorithmsInfo from './components/AlgorithmsInfo.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        {/* main app - home and other internal pages */}
+        <Route path="/*" element={<App />} />
+
+        {/* standalone algorithms info page */}
+        <Route
+          path="/algorithms"
+          element={<AlgorithmsInfo />}
+          id="algorithms-page"
+        />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
